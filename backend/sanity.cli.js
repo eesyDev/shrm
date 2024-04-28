304f59a9eea27075f8aa6fca9381b4b0fbed8096
ranokhassanova <khasanova.rano@gmail.com>
2024-04-28 21:41:00 +0500

    first commit

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'bfmg3w4h',
+    dataset: 'production'
+  }
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route, useNavigate, BrowserRouter } from 'react-router-dom';
+
+import Home from './container/Home';
+import Login from './components/Login';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<Home/>}/>
+        <Route path='login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
